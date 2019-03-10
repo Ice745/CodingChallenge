0d823ed4a44ec2e18df924cfb8595c9d10869e73
Setriakor <setriakor@gmail.com>
2019-03-10 21:25:28 +0000

    test commit

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,11 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Graph {
 
+    private Set<Airport> nodes = new HashSet<>();
+
+    public void addNode(Airport nodeA) {
+        nodes.add(nodeA);
+    }
 }
