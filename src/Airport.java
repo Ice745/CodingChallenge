4964d709ab7eae38a4f5312818408fbcaf8544b1
Setriakor <setriakor@gmail.com>
2019-03-10 21:16:49 +0000

    test commit

---
+++ src/Airport.java
@@ -0,0 +1,85 @@
+public class Airport {
+    int id;
+    String name;
+    String city;
+    String country;
+    String iata;
+    String icao;
+    double lat;
+    double lng;
+
+    public Airport(int id, String name, String city, String country, String iata, String icao, double lat, double lng) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.country = country;
+        this.iata = iata;
+        this.icao = icao;
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getIata() {
+        return iata;
+    }
+
+    public void setIata(String iata) {
+        this.iata = iata;
+    }
+
+    public String getIcao() {
+        return icao;
+    }
+
+    public void setIcao(String icao) {
+        this.icao = icao;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+}
