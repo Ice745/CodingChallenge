22089dfab54e78acf195aff84c668ec0889eed04
Ice745 <iclementowusu@gmail.com>
2019-03-10 01:28:08 +0000

    First commit

---
+++ src/CodeChallenge/Challenge.java
@@ -0,0 +1,40 @@
+package CodeChallenge;
+
+import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+
+public class Challenge {
+    public Challenge(){}
+
+    public static void main(String args[]){
+        try{
+            String routes = "routes.csv";
+            String airlines = "airlines.csv";
+            String airports = "airports.csv";
+            FileInputStream file_routes = new FileInputStream("C:\\Users\\Isaac\\Documents\\Code\\CodeChallenge\\"+routes);
+            FileInputStream file_airports = new FileInputStream("C:\\Users\\Isaac\\Documents\\Code\\CodeChallenge\\"+airports);
+            FileInputStream file_airlines = new FileInputStream("C:\\Users\\Isaac\\Documents\\Code\\CodeChallenge\\"+airlines);
+            BufferedInputStream binRoute = new BufferedInputStream(file_routes);
+            BufferedInputStream binAirport = new BufferedInputStream(file_airports);
+            BufferedInputStream binAirline = new BufferedInputStream(file_airlines);
+            int i;
+            while((i=binRoute.read())!=-1){
+                System.out.print((char)i);
+            }
+            binRoute.close();
+            file_routes.close();
+            while((i=binAirport.read())!=-1){
+                System.out.print((char)i);
+            }
+            binAirport.close();
+            file_airports.close();
+            while((i=binAirline.read())!=-1){
+                System.out.print((char)i);
+            }
+            binAirline.close();
+            file_airlines.close();
+        }catch(Exception e){System.out.println(e);}
+    }
+}
